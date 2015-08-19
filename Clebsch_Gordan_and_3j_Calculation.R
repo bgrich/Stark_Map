290a3cba225780f18b6c875b131764cd6815a955
bgrich <brian.g.richards@gmail.com>
2015-08-19 11:39:14 -0400

    Updated to include code to determine the Clebsch-Gordan Coefficient using the definition from Bob's Notes (variation on the definition from Edmonds).

--- Clebsch_Gordan_and_3j_Calculation.R
+++ Clebsch_Gordan_and_3j_Calculation.R
@@ -1,2 +1,35 @@
-#Calculating the 3j symbol and Clebsch-Gordan coefficient for the case of j3 = j = j1 + j2 and m3 = -mj = -m1 - m2
+#Calculating the 3j symbol and Clebsch-Gordan coefficient
 
+Clebsch_Gordan <- function(j1,j2,m1,m2,j,mj){
+  
+  if((m1+m2)!=mj){
+    CG <- 0
+  } else {
+    NotSumNumerator <- sqrt((2*j+1)*factorial(j1+j2-j)*factorial(j1-m1)*factorial(j2-m2)*factorial(j+mj)*factorial(j-mj))
+    
+    NotSumDenominator <- sqrt(factorial(j1+j2+j+1)*factorial(j1-j2+j)*factorial(-j1+j2+j)*factorial(j1+m1)*factorial(j2+m2))
+    
+    s <- 0
+    sum <- 0
+    sum_s <- 0
+    repeat{
+      D1 <- s
+      D2 <- j1-m1-s
+      D3 <- j-mj-s
+      D4 <- j2-j+m1+s
+      
+      if((D1<0)|(D2<0)|(D3<0)|(D4<0)){
+        break
+      } else{
+        
+        sum_s <- (-1)^(s+j1-m1)*factorial(j1+m1+s)*factorial(j2+j-m1-s)/(factorial(D1)*factorial(D2)*factorial(D3)*factorial(D4))
+        
+        sum <- sum+sum_s
+        s <- s+1
+      }
+    }
+    CG <- (NotSumNumerator/NotSumDenominator)*sum
+  }
+  
+  CG
+}
