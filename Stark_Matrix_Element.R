513e8e36b64b30fcecf2a000159a254f38378fa0
bgrich <brian.g.richards@gmail.com>
2015-08-19 17:41:00 -0400

    Corrected typo's in code. Still need to test and determine a way to deal with NaN's that appear for certain cases.

--- Stark_Matrix_Element.R
+++ Stark_Matrix_Element.R
@@ -6,7 +6,7 @@ StarkMatrixElem <- function(n1, n2, l1, l2, j1, j2, mj1, mj2, Field){
   } else{
     if((l2==(l1+1))|(l2==(l1-1))){
       
-      StarkElem <- RadialMatrixElement(1,n1,n2,l1,l2,j1,j2)*Field*(Clebsch_Gordan(l1,1/2,mj1+1/2,-1/2,j1,mj1)*Clebsch_Gordan(l1,1/2,mj1+1/2,-1/2,j1,mj1)*SphereMatElement(l1,l2,mj1+1/2)+Clebsch_Gordan(l1,1/2,mj1-1/2,1/2,j1,mj1)*Clebsch_Gordan(l1,1/2,mj1-1/2,1/2,j1,mj1)*SphereMatElement(l1,l2,mj1-1/2))
+      StarkElem <- RadialMatrixElement(1,n1,n2,l1,l2,j1,j2)*Field*(Clebsch_Gordan(l1,1/2,mj1+1/2,-1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1+1/2,-1/2,j2,mj1)*SphereMatElement(l1,l2,mj1+1/2)+Clebsch_Gordan(l1,1/2,mj1-1/2,1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1-1/2,1/2,j2,mj1)*SphereMatElement(l1,l2,mj1-1/2))
       
     } else{
       StarkElem <- 0
