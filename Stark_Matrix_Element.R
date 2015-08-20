393aa0036d46cd1c9fb7b74bd685d8536c854bbd
bgrich <brian.g.richards@gmail.com>
2015-08-20 11:20:36 -0400

    Updated to allow for cases where the spherical harmonic matrix element may be zero. Spherical harmonic is calculated first and if it is zero, sets the whole term to zero.

--- Stark_Matrix_Element.R
+++ Stark_Matrix_Element.R
@@ -6,7 +6,26 @@ StarkMatrixElem <- function(n1, n2, l1, l2, j1, j2, mj1, mj2, Field){
   } else{
     if((l2==(l1+1))|(l2==(l1-1))){
       
-      StarkElem <- RadialMatrixElement(1,n1,n2,l1,l2,j1,j2)*Field*(Clebsch_Gordan(l1,1/2,mj1+1/2,-1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1+1/2,-1/2,j2,mj1)*SphereMatElement(l1,l2,mj1+1/2)+Clebsch_Gordan(l1,1/2,mj1-1/2,1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1-1/2,1/2,j2,mj1)*SphereMatElement(l1,l2,mj1-1/2))
+      if(SphereMatElement(l1,l2,mj1+1/2)==0){
+        
+      SumPlus <- 0
+      
+      } else{
+        
+        SumPlus <- Clebsch_Gordan(l1,1/2,mj1+1/2,-1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1+1/2,-1/2,j2,mj1)*SphereMatElement(l1,l2,mj+1/2)
+        
+      }
+      if(SphereMatElement(l1,l2,mj1-1/2)==0){
+        
+        SumMinus <- 0
+        
+      } else{
+        
+        SumMinus <- Clebsch_Gordan(l1,1/2,mj1-1/2,1/2,j1,mj1)*Clebsch_Gordan(l2,1/2,mj1-1/2,1/2,j2,mj1)*SphereMatElement(l1,l2,mj1-1/2)
+        
+      }
+            
+      StarkElem <- RadialMatrixElement(1,n1,n2,l1,l2,j1,j2)*Field*(SumPlus + SumMinus)
       
     } else{
       StarkElem <- 0
