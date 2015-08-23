3de4ee3ef443dfae813c0a3967bc6b90fd272c02
Brian Richards <brian.g.richards@gmail.com>
2015-08-23 14:13:55 -0400

    Updated code and new files for n equals 13 to 20 for m_j equals 1/2.

--- Combined_Stark_Map_Script.R
+++ Combined_Stark_Map_Script.R
@@ -1,7 +1,7 @@
-n <- c(13,14,15,16,17,18)
+# n <- c(13,14,15,16,17,18,19,20)
 # l <- c(0,1,2,3,4,5,6,7)
 # n <- c(15,16)
-
+n <- c(25,26,27,28,29,30,31,32,33,34,35)
 
 NumberMatrix <- numeric()
 
@@ -52,7 +52,7 @@ for(k in 1:length(field)){
   Energy <- rbind(Energy, temp$values)
 }
 
-png(paste("Stark_Map_for_n_",min(n),"_to_", max(n),",png",sep=''), width = 1920, height = 1080)
+png(paste("Stark_Map_for_n_",min(n),"_to_", max(n),".png",sep=''), width = 1920, height = 1080)
 plot(c(min(field), max(field)),c(-540,-440), col=0)
 for(i in 1:ncol(Energy)){
   lines(field, Energy[,i]*2.19475e5)
@@ -69,7 +69,7 @@ for(i in 1:size){
 }
 ZeroEnergyDataFrame <- tbl_df(ZeroEnergyDataFrame)
 
-test.frame2 <- ZeroEnergyDataFrame%>%arrange(E0, l)
+test.frame2 <- ZeroEnergyDataFrame%>%arrange(desc(E0), desc(l))
 
 
 EnergyDataFrame <- data.frame(Field = numeric(), E = numeric(), E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), state = character())
@@ -86,9 +86,10 @@ EnergyDataFrame <- tbl_df(EnergyDataFrame)
 
 write.csv(EnergyDataFrame, paste("Tidy_Stark_Energy_Output_n_",min(n),"_to_",max(n),".csv", sep = ''), row.names = FALSE)
 
-# 
-# EnergyDataFrame%>%
-#   group_by(state)%>%
-#   ggplot(aes(x=Field, y = E*2.19475e5, group=state))+
-#   geom_line()+
-#   scale_y_continuous(limits=c(-740,-600))
+
+EnergyDataFrame%>%
+  group_by(state)%>%
+  ggplot(aes(x=Field, y = E*2.19475e5, group=state))+
+  geom_line()+
+  geom_line(data=(EnergyDataFrame%>%filter(state=="18,0,0.5")), color= "red")+
+  scale_y_continuous(limits=c(-540,-440))
