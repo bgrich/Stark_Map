18a2273815fc50c8f0d61e0d737459d13e654aa9
bgrich <brian.g.richards@gmail.com>
2015-08-24 10:43:28 -0400

    Included comments and cleaned up code. Included arbitrary mj calculation.

--- Combined_Stark_Map_Script.R
+++ Combined_Stark_Map_Script.R
@@ -1,15 +1,18 @@
+
+#Sets the n and mj levels for the Stark Matrix
 # n <- c(13,14,15,16,17,18,19,20)
-# l <- c(0,1,2,3,4,5,6,7)
-# n <- c(15,16)
 n <- c(25,26,27,28,29,30,31,32,33,34,35)
+mj <- 1/2
 
+#Initializes and fills a matrix with all of the n, l, and j states for the Stark matrix
 NumberMatrix <- numeric()
-
+#The lowest l level is determined by l = mj - 1/2. For that l level, there is only a single j state j = mj. For all higher l's, there are two j states, j = l +/- 1/2.
 for(i in 1:length(n)){
-  l <- 0
-  for(j in 1:(n[i]-1)){
-    if(l==0){
-      NumberMatrix <- rbind(NumberMatrix, c(n[i],l,1/2))  
+  l0 <- mj - 1/2
+  l <- l0
+  for(j in l0:(n[i]-1)){
+    if(l == l0){
+      NumberMatrix <- rbind(NumberMatrix, c(n[i],l,mj))
     } else{
       NumberMatrix <- rbind(NumberMatrix, c(n[i],l,l-1/2))
       NumberMatrix <- rbind(NumberMatrix, c(n[i],l,l+1/2))
@@ -18,6 +21,7 @@ for(i in 1:length(n)){
   }
 }
 
+#Breaks the matrix of all possible states into several vectors for each n1, n2, l1, l2, j1, and j2. Also determines the size of the matrix.
 n1 <- NumberMatrix[,1]
 n2 <- NumberMatrix[,1]
 l1 <- NumberMatrix[,2]
@@ -26,77 +30,73 @@ j1 <- NumberMatrix[,3]
 j2 <- NumberMatrix[,3]
 size <- length(n1)
 
-mat <- matrix(, nrow = size, ncol = size)
-
+#Initializes and computes the Stark Matrix
+StarkMatrix <- matrix(, nrow = size, ncol = size)
+#Fills the Stark matrix. Treats the Stark matrix as symmetric and computes only the elements for the upper right triangle of the matrix. Copies those into the symmetric terms on the lower left triangle of the matrix.
 for(i in 1:size){
   for(j in i:size){
-    mat[i,j] <- StarkMatrixElem(n1[i],n2[j],l1[i],l2[j],j1[i],j2[j],1/2,1/2)
-    mat[j,i] <- mat[i,j]
+    StarkMatrix[i,j] <- StarkMatrixElem(n1[i],n2[j],l1[i],l2[j],j1[i],j2[j],1/2,1/2)
+    StarkMatrix[j,i] <- StarkMatrix[i,j]
     print(paste("i = ", i, ", j = ", j, sep = ''))
   }
 }
 
-write.csv(mat, paste("Stark_Matrix_Output_",min(n),"to",max(n),".csv", sep = ''), row.names = FALSE)
+#Saves the Stark matrix to file
+write.csv(StarkMatrix, paste("Output_Files/Stark_Matrix_Output_",min(n),"_to_",max(n),"_mj_" ,mj,".csv", sep = ''), row.names = FALSE)
 
+#Initializes and fills a matrix for the energy at zero electric field.
 ZeroFieldEnergy <- matrix(0,nrow = size, ncol = size)
 
 for(i in 1:size){
   ZeroFieldEnergy[i,i] <- -1/(n1[i]- QuantumDefect(n1[i],l1[i],j1[i]))^2/2
 }
 
+#Determines the number of electric field points and the step size.
 field <- seq(0,6000, by = 5)
 field.au <- field/5.142e9
 Energy <- numeric()
 
+#Diagonalizes a matrix of the zero field energy + the Stark matrix times the electric field. Also builds a matrix of the eigenvalues
 for(k in 1:length(field)){
-  temp <- eigen(ZeroFieldEnergy+mat*field.au[k])
-  Energy <- rbind(Energy, temp$values)
+  Energy.newRow <- eigen(ZeroFieldEnergy+StarkMatrix*field.au[k])
+  Energy <- rbind(Energy, Energy.newRow$values)
 }
 
-png(paste("Stark_Map_for_n_",min(n),"_to_", max(n),".png",sep=''), width = 1920, height = 1080)
-plot(c(min(field), max(field)),c(-540,-440), col=0)
-for(i in 1:ncol(Energy)){
-  lines(field, Energy[,i]*2.19475e5)
-}
-dev.off()
+#Outputs the Stark energy data frame to file.
+write.csv(Energy, paste("Output_Files/Stark_Energy_Output_",min(n),"_to_",max(n),"_mj_" ,mj,".csv", sep = ''), row.names = FALSE)
 
+##Data tidying
+#Creates a data frame with the zero field energy and the related n,l,j,mj and a string representing the entire state.
 
-ZeroEnergyDataFrame <- data.frame(E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), state = character())
+ZeroEnergyDataFrame <- data.frame(E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), mj = numeric(), state = character())
 
 for(i in 1:size){
-  ZeroEnergy.newrow <- data.frame(E0 = -1/(n1[i]- QuantumDefect(n1[i],l1[i],j1[i]))^2/2, n = n1[i], l = l1[i], j = j1[i], state = paste(n1[i],l1[i], j1[i], sep = ','))
+  ZeroEnergy.newrow <- data.frame(E0 = -1/(n1[i]- QuantumDefect(n1[i],l1[i],j1[i]))^2/2, n = n1[i], l = l1[i], j = j1[i],mj = mj, state = paste(n1[i],l1[i], j1[i],mj, sep = ','))
   
   ZeroEnergyDataFrame <- rbind(ZeroEnergyDataFrame, ZeroEnergy.newrow)
 }
+#Turns the data frame in to a dplyr table.
 ZeroEnergyDataFrame <- tbl_df(ZeroEnergyDataFrame)
 
+#Determines which direction the Energy eigenvalues are going and arranges the zero energy data frame to match. If the minimum zero field energy is the last column, then the Zero Field data frame is put in descending order. If the min zero field energy is the first column, then the Zero Field data frame is put in ascending order.
 if(which.min(Energy[1,])>1){
-  test.frame2 <- ZeroEnergyDataFrame%>%arrange(desc(E0), desc(l))  
+  ZeroFieldDataFrame <- ZeroEnergyDataFrame%>%arrange(desc(E0), desc(l))  
 } else{
-  test.frame2 <- ZeroEnergyDataFrame%>%arrange(E0, l)
+  ZeroFieldDataFrame <- ZeroEnergyDataFrame%>%arrange(E0, l)
 }
 
-test.frame2 <- ZeroEnergyDataFrame%>%arrange(desc(E0), desc(l))
+EnergyDataFrame <- data.frame(Field = numeric(), E = numeric(), E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), mj = numeric(), state = character())
 
-
-EnergyDataFrame <- data.frame(Field = numeric(), E = numeric(), E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), state = character())
-
+#Creates a tidy data frame of the Energy eigen states at all fields
 for(k in 1:length(Energy[1,])){
   print(k)
   for(i in 1:length(field)){
-    EnergyDataFrame.newrow <- data.frame(Field = field[i],E = Energy[i,k],E0 = test.frame2$E0[k], n = test.frame2$n[k], l = test.frame2$l[k], j = test.frame2$j[k], state = test.frame2$state[k])
+    EnergyDataFrame.newrow <- data.frame(Field = field[i],E = Energy[i,k],E0 = ZeroFieldDataFrame$E0[k], n = ZeroFieldDataFrame$n[k], l = ZeroFieldDataFrame$l[k], j = ZeroFieldDataFrame$j[k], mj = ZeroFieldDataFrame$mj[k], state = ZeroFieldDataFrame$state[k])
     
     EnergyDataFrame <- rbind(EnergyDataFrame, EnergyDataFrame.newrow)
   }
 }
 EnergyDataFrame <- tbl_df(EnergyDataFrame)
 
-write.csv(EnergyDataFrame, paste("Tidy_Stark_Energy_Output_n_",min(n),"_to_",max(n),".csv", sep = ''), row.names = FALSE)
-
-
-EnergyDataFrame%>%
-  group_by(state)%>%
-  ggplot(aes(x=Field, y = E*2.19475e5, group=state))+
-  geom_line()+
-  geom_line(data=(EnergyDataFrame%>%filter(state=="18,0,0.5")), color= "red")+
-  scale_y_continuous(limits=c(-540,-440))
+#Writes tidy data frame to file.
+write.csv(EnergyDataFrame, paste("Output_Files/Tidy_Stark_Energy_Output_n_",min(n),"_to_",max(n),"_mj_",mj,".csv", sep = ''), row.names = FALSE)
