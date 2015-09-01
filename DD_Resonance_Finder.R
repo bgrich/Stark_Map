d125b72b475eed3db3a6411266a8e36d8984b343
bgrich <brian.g.richards@gmail.com>
2015-09-01 10:54:00 -0400

    Fixed a typo in dipole-dipole resonance finder function. Created plots of the 32p state and 32p, 32s, and 33s states.

--- DD_Resonance_Finder.R
+++ DD_Resonance_Finder.R
@@ -1,19 +1,19 @@
-EnergyDataFrame <- tbl_df(read.csv("Output_Files/QDAdj_Tidy_Stark_Energy_Output_n_27_to_34_mj_0.5.csv"))
+EnergyDataFrame1 <- tbl_df(read.csv("Output_Files/Tidy_Stark_Energy_n_27_to_34_mj_0.5_to_3.5.csv"))
 
-EnergyDataFrame <- EnergyDataFrame %>%
+EnergyDataFrame1 <- EnergyDataFrame1 %>%
   tbl_df() %>%
   mutate(Ecm = E*2.19475e5)
 
 #Function for accepting an arbitrary state input and a given data frame and outputing the Zero crossings for the case 2*A - (B + C)
 
 DDRes <- function(Frame, StateA, StateB, StateC){
-  A <- EnergyDataFrame %>%
+  A <- Frame %>%
     filter(state %in% StateA) %>%
     select(Field, Ecm)
-  B <- EnergyDataFrame %>%
+  B <- Frame %>%
     filter(state == StateB) %>%
     select(Field, Ecm)
-  C <- EnergyDataFrame %>%
+  C <- Frame %>%
     filter(state == StateC) %>%
     select(Field, Ecm)
 
@@ -22,15 +22,16 @@ DDRes <- function(Frame, StateA, StateB, StateC){
 }
 
 
-a <- EnergyDataFrame %>%
+a <- EnergyDataFrame2 %>%
   filter(state %in% "32,1,1.5,0.5") %>%
   select(Field, Ecm)
-b <- EnergyDataFrame %>%
+b <- EnergyDataFrame2 %>%
   filter(state == "32,0,0.5,0.5") %>%
   select(Field, Ecm)
-c <- EnergyDataFrame %>%
+c <- EnergyDataFrame2 %>%
   filter(state == "33,0,0.5,0.5") %>%
   select(Field, Ecm)
 
 ZeroCross(a$Field, (2*a$Ecm - (b$Ecm + c$Ecm)))
 
+plot(a$Field, (2*a$Ecm - (b$Ecm + c$Ecm)))
--- Plots_Script.R
+++ Plots_Script.R
@@ -1,16 +1,47 @@
-highlight <- TidyEnergy1 %>%
-  filter(Ecm > -135, Ecm < -121, Field < 100) %>%
-  filter(state %in% c("32,1,1.5,0.5" , "33,0,0.5,0.5" , "32,0,0.5,0.5"))
+highlight <- EnergyDataFrame2 %>%
+  filter(Ecm > -128, Ecm < -127, Field < 100) %>%
+  filter(state %in% c("32,1,1.5,1.5"))
 
-TidyEnergy1%>%
+PStateGraph <- EnergyDataFrame2%>%
   group_by(state)%>%
-  filter(Ecm > -135, Ecm < -121, Field < 100) %>%
+  filter(Ecm > -128, Ecm < -127, Field < 100) %>%
   ggplot(aes(x=Field, y = Ecm, group=state))+
-  geom_line()+
-  geom_line(data=highlight, color= "red")+
-  theme(axis.text = element_text(size = 20))
+  geom_line(size = 2)+
+  geom_line(data=highlight, color= "red", size = 2)+
+  ggtitle("32P states for Rb-85")+
+  xlab("Field (V/cm)")+
+  ylab("Energy (cm^-1)")+
+  theme_bw()+
+  theme(axis.text = element_text(size = 35), plot.title = element_text(size = 35), axis.title = element_text(size = 35))
 
-test1 <- TidyEnergy1 %>%
-  group_by(Field)
-filter(Ecm > -135, Ecm < -121, Field < 100) %>%
-  filter(state %in% "32,1,1.5,0.5") %>%
+png("Output_Plots/32pStates.png", width = 1920, height = 1080)
+PStateGraph + annotate("text", x = 0, y = -127.33, label = "32p_3/2", size = 10)+
+  annotate("text", x = 0, y = -127.45, label = "32p_1/2", size = 10)+
+  annotate("text", x = 20, y = -127.36, label = "|mj| = 3/2", size = 10)+
+  annotate("text", x = 15, y = -127.40, label = "|mj| = 1/2", size = 10)
+dev.off()
+  
+  
+
+highlight2 <- EnergyDataFrame2 %>%
+  filter(state %in% c("32,1,1.5,1.5" , "33,0,0.5,0.5" , "32,0,0.5,0.5"))
+
+DDResPlot <- EnergyDataFrame2 %>%
+  group_by(state)%>%
+  filter(Ecm > -133, Ecm < -122.5) %>%
+  ggplot(aes(x = Field, y = Ecm, group = state))+
+  geom_line()+
+  geom_line(data = highlight2, color = "red", size = 2)+
+  ggtitle("32P, 32s, and 33s states for Rb-85")+
+  xlab("Field (V/cm)")+
+  ylab("Energy (cm^-1)")+
+  theme_bw()+
+  theme(axis.text = element_text(size = 35), plot.title = element_text(size = 35), axis.title = element_text(size = 35))
+
+png("DDRes_32p_32s_33s.png", width = 1920, height = 1080)
+DDResPlot + 
+  annotate("text", x = 0, y = -127, label = "32p", size = 10)+
+  annotate("text", x = 0, y = -130, label = "n = 29", size = 10)+
+  annotate("text", x = 0, y = -131.3, label = "32s", size = 10)+
+  annotate("text", x = 0, y = -123.5, label = "33s", size = 10)
+dev.off()
