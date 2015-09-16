b7747fca5a7f2e64dfaf427b9589a1626fa6f56d
bgrich <brian.g.richards@gmail.com>
2015-09-16 15:46:05 -0400

    Fixed Combined_Stark_Map_Script_Adj.

--- Combined_Stark_Map_Script_Adj.R
+++ Combined_Stark_Map_Script_Adj.R
@@ -51,20 +51,20 @@ StarkMatrix <- matrix(, nrow = size, ncol = size)
 #Fills the Stark matrix. Treats the Stark matrix as symmetric and computes only the elements for the upper right triangle of the matrix. Copies those into the symmetric terms on the lower left triangle of the matrix.
 for(i in 1:size){
   for(j in i:size){
-    StarkMatrix[i,j] <- StarkMatrixElemAdjusted(n1[i],n2[j],l1[i],l2[j],j1[i],j2[j],mj,mj)
+    StarkMatrix[i,j] <- StarkMatrixElem(n1[i],n2[j],l1[i],l2[j],j1[i],j2[j],mj,mj)
     StarkMatrix[j,i] <- StarkMatrix[i,j]
     print(paste("i = ", i, ", j = ", j, sep = ''))
   }
 }
 
 #Saves the Stark matrix to file
-write.csv(StarkMatrix, paste("Output_Files/QDAdjStark_Matrix_Output_",min(n),"_to_",max(n),"_mj_" ,mj,".csv", sep = ''), row.names = FALSE)
+write.csv(StarkMatrix, paste("Output_Files/Stark_Matrix_Output_",min(n),"_to_",max(n),"_mj_" ,mj,".csv", sep = ''), row.names = FALSE)
 
 #Initializes and fills a matrix for the energy at zero electric field.
 ZeroFieldEnergy <- matrix(0,nrow = size, ncol = size)
 
 for(i in 1:size){
-  ZeroFieldEnergy[i,i] <- -1/(n1[i]- QuantumDefectAdjusted(n1[i],l1[i],j1[i]))^2/2
+  ZeroFieldEnergy[i,i] <- -1/(n1[i]- QuantumDefect(n1[i],l1[i],j1[i]))^2/2
 }
 
 #Determines the number of electric field points and the step size.
@@ -80,7 +80,7 @@ for(k in 1:length(field)){
 }
 
 #Outputs the Stark energy data frame to file.
-write.csv(Energy, paste("Output_Files/QDAdj_Stark_Energy_Output_",min(n),"_to_",max(n),"_mj_" ,mj,".csv", sep = ''), row.names = FALSE)
+write.csv(Energy, paste("Output_Files/Stark_Energy_Output_",min(n),"_to_",max(n),"_mj_" ,mj,".csv", sep = ''), row.names = FALSE)
 
 ##Data tidying
 #Creates a data frame with the zero field energy and the related n,l,j,mj and a string representing the entire state.
@@ -88,7 +88,7 @@ write.csv(Energy, paste("Output_Files/QDAdj_Stark_Energy_Output_",min(n),"_to_",
 ZeroEnergyDataFrame <- data.frame(E0 = numeric(), n = numeric(), l = numeric(), j = numeric(), mj = numeric(), state = character())
 
 for(i in 1:size){
-  ZeroEnergy.newrow <- data.frame(E0 = -1/(n1[i]- QuantumDefectAdjusted(n1[i],l1[i],j1[i]))^2/2, n = n1[i], l = l1[i], j = j1[i],mj = mj, state = paste(n1[i],l1[i], j1[i],mj, sep = ','))
+  ZeroEnergy.newrow <- data.frame(E0 = -1/(n1[i]- QuantumDefect(n1[i],l1[i],j1[i]))^2/2, n = n1[i], l = l1[i], j = j1[i],mj = mj, state = paste(n1[i],l1[i], j1[i],mj, sep = ','))
   
   ZeroEnergyDataFrame <- rbind(ZeroEnergyDataFrame, ZeroEnergy.newrow)
 }
@@ -111,4 +111,4 @@ for(k in 1:length(Energy[1,])){
 }
 
 #Writes tidy data frame to file.
-write.csv(EnergyDataFrame, paste("Output_Files/QDAdj_Tidy_Stark_Energy_Output_n_",min(n),"_to_",max(n),"_mj_",mj,".csv", sep = ''), row.names = FALSE)
+write.csv(EnergyDataFrame, paste("Output_Files/Tidy_Stark_Energy_Output_n_",min(n),"_to_",max(n),"_mj_",mj,".csv", sep = ''), row.names = FALSE)
